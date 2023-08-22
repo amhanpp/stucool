042401f35afe541e0c9f199dbc62c12953a7155e
amhanpp <amhanpp@gmail.com>
2023-08-22 18:21:11 +0800

    Update python.js

--- python.js
+++ python.js
@@ -1,7 +1,7 @@
 /*******************************
 脚本名称:  PYTHON手册破解
 更新时间：2023年8月22日 下午6:30
-
+感谢大佬 自留备份
 *******************************
 [rewrite_local]
 ^http[s]?:\/\/book.haoapp8.cn\/API\/programming\/v5\/booklist.php url script-response-body https://raw.githubusercontent.com/stucool/python.js
