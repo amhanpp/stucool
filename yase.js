d749267b987ead5ed4f7bcd27ebd0a30fe251f0f
amhanpp <amhanpp@gmail.com>
2023-09-14 14:02:31 +0800

    Update yase.js

--- yase.js
+++ yase.js
@@ -15,8 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/yase.js
-
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/amhanpp/stucool/main/yase.js
 
 [MITM]
 
