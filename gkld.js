20daf41ecda20011a8a5a19e2903d9a5cdf06e97
amhanpp <amhanpp@gmail.com>
2023-09-15 17:46:19 +0800

    Update gkld.js

--- gkld.js
+++ gkld.js
@@ -23,15 +23,15 @@ https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/XiaoMaoG
 hostname = errlog.umeng.com, api.gongkaoleida.com
 
 [rewrite_local]
-https:\/\/api\.gongkaoleida\.com\/api\/v5_4_4\/exam\/jobSearchList url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/gkld.js
-https:\/\/api\.gongkaoleida\.com\/api\/v5_4_0\/exam\/getExamList url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/gkld.js
+https:\/\/api\.gongkaoleida\.com\/api\/v5_4_4\/exam\/jobSearchList url script-response-body https://raw.githubusercontent.com/amhanpp/stucool/main/gkld.js
+https:\/\/api\.gongkaoleida\.com\/api\/v5_4_0\/exam\/getExamList url script-response-body https://raw.githubusercontent.com/amhanpp/stucool/main/gkld.js
 https:\/\/errlog\.umeng\.com\/api\/crashsdk\/logcollect url reject
 https:\/\/api\.gongkaoleida\.com\/api\/v5\/my\/index url reject
-https:\/\/api\.gongkaoleida\.com\/api\/v5_4_3\/sets\/getFilterResult url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/gkld.js
+https:\/\/api\.gongkaoleida\.com\/api\/v5_4_3\/sets\/getFilterResult url script-response-body https://raw.githubusercontent.com/amhanpp/stucool/main/gkld.js
 https:\/\/api\.gongkaoleida\.com\/api\/v4_1_2\/sets\/getAllTips url reject
 https:\/\/api\.gongkaoleida\.com\/api\/v2\/ad\/info url reject
-https:\/\/api\.gongkaoleida\.com\/api\/v5_4_3\/my\/vipCardInfo url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/gkld.js
-https:\/\/api\.gongkaoleida\.com\/api\/v5_3_7\/user\/getInfo url script-response-body https://raw.githubusercontent.com/xiaomaoJT/QxScript/main/rewrite/boxJS/source/gkld.js
+https:\/\/api\.gongkaoleida\.com\/api\/v5_4_3\/my\/vipCardInfo url script-response-body https://raw.githubusercontent.com/amhanpp/stucool/main/gkld.js
+https:\/\/api\.gongkaoleida\.com\/api\/v5_3_7\/user\/getInfo url script-response-body https://raw.githubusercontent.com/amhanpp/stucool/main/gkld.js
 https:\/\/member\.aliyundrive\.com\/v1\/config\/customCapsule url reject-dict
 
  ***************/
