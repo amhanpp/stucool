40ad24013dd054598070b81ff9054555d1e7e7e3
amhanpp <amhanpp@gmail.com>
2023-10-08 20:01:38 +0800

    Update 6.js

--- 6.js
+++ 6.js
@@ -9,8 +9,8 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Revenuecat/Vision.js
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/Revenuecat/Vision.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/amhanpp/stucool/main/6.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/amhanpp/stucool/main/6.js
 
 [mitm]
 hostname = api.revenuecat.com
