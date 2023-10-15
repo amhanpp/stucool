fb1e1d8ee8a5883231357d90b5b876c800b530b7
amhanpp <amhanpp@gmail.com>
2023-10-15 09:37:27 +0800

    Update and rename jianshen1.js to gdwxinzhi.js

--- gdwxinzhi.js
+++ gdwxinzhi.js
@@ -25,9 +25,9 @@ hostname = gdw.xinzhi.space
 *******************************/
 
 var body = $response.body.replace(/is_vip":\d+/g,'is_vip":1')
-。replace(/vip_type":""/g,'vip_type":"vip"')
-。replace(/vip_day":\d+/g,'vip_day":99999')
-。replace(/vip_expire":""/g,'vip_expire":"您当前为终身VIP，会员永不"')
-。replace(/vip_expire":null/g,'vip_expire":"您当前为终身VIP，会员永不"')
-。replace(/nickname":".*?"/g,'nickname":"彭于晏解锁"')
+。replace(/vip_type":""/g，'vip_type":"vip"')
+。replace(/vip_day":\d+/g，'vip_day":99999')
+。replace(/vip_expire":""/g，'vip_expire":"您当前为终身VIP，会员永不"')
+。replace(/vip_expire":null/g，'vip_expire":"您当前为终身VIP，会员永不"')
+。replace(/nickname":".*?"/g，'nickname":"彭于晏解锁"')
 $done({ body });
