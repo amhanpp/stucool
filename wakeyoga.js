8a1f1bd29b01da628caa906e00090d48ce6110c3
amhanpp <amhanpp@gmail.com>
2023-10-15 08:46:53 +0800

    Update wakeyoga.js

--- wakeyoga.js
+++ wakeyoga.js
@@ -15,7 +15,8 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^http[s]?:\/\/api.wakeyoga.com url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/wakeyoga.js
+
+^http[s]?:\/\/api.wakeyoga.com url script-response-body https://raw.githubusercontent.com/amhanpp/stucool/main/wakeyoga.js
 
 [mitm] 
 
@@ -24,16 +25,16 @@ hostname = api.wakeyoga.com
 *******************************/
 
 var body = $response.body.replace(/"isOrderSvip":\w+/g,'"isOrderSvip":true')
-.replace(/"uSvipExpireAt":\d+/g,'"uSvipExpireAt":4567891456')
-.replace(/"lifetimeMembershipStatus":\d/g,'"lifetimeMembershipStatus":1')
-.replace(/"canPlay":\w+/g,'"canPlay":1')
-.replace(/"user_has":\w+/g,'"user_has":1')
-.replace(/"order_status":\d/g,'"order_status":1')
-.replace(/"order_expire_at":null/g,'"order_expire_at":4567891456')
-.replace(/"canPlay":\d/g,'"canPlay":1')
-.replace(/"vipType":\d/g,'"vipType":1')
-.replace(/"isBuy":\d/g,'"isBuy":1')
-.replace(/"isSvip":\d/g,'"isSvip":1')
+。replace(/"uSvipExpireAt":\d+/g,'"uSvipExpireAt":4567891456')
+。replace(/"lifetimeMembershipStatus":\d/g,'"lifetimeMembershipStatus":1')
+。replace(/"canPlay":\w+/g,'"canPlay":1')
+。replace(/"user_has":\w+/g,'"user_has":1')
+。replace(/"order_status":\d/g,'"order_status":1')
+。replace(/"order_expire_at":null/g,'"order_expire_at":4567891456')
+。replace(/"canPlay":\d/g,'"canPlay":1')
+。replace(/"vipType":\d/g,'"vipType":1')
+。replace(/"isBuy":\d/g,'"isBuy":1')
+。replace(/"isSvip":\d/g,'"isSvip":1')
 .replace(/"isCantplay":\d/g,'"isCantplay":0')
 .replace(/"unloakDay":\d+/g,'"unloakDay":99')
 $done({ body });
