83272da42def424cceddbde0bc5f932a6f4439e0
amhanpp <amhanpp@gmail.com>
2023-10-15 08:53:01 +0800

    Update and rename you.js to You.js

--- You.js
+++ You.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/amhanpp/stucool/main/You.js
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/amhanpp/stucool/main/ou.js
 
 [mitm] 
 
